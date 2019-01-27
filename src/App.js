ca1870d9180861280da91f10fa8a3ddf97c7c18b
Alimzhan Aigarayev <alimzhan.aigarayev@gmail.com>
2019-01-27 21:05:44 +0100

    petfinder keys added in .env

--- src/App.js
+++ src/App.js
@@ -1,35 +1,30 @@
 import React from "react";
 import { render } from "react-dom";
+import pf from "petfinder-client";
 import { Pet } from "./Pet";
 
+// Keys that used in .env file
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
+
 class App extends React.Component {
-  handleTitleClick() {
-    alert("You clicked the title");
+  componentDidMount() {
+    const promise = petfinder.breed.list({ animal: "dog" });
+
+    promise.then(console.log, console.error);
   }
 
   render() {
-    return React.createElement("div", {}, [
-      React.createElement(
-        "h1",
-        { onClick: this.handleTitleClick },
-        "Do Something!"
-      ),
-      React.createElement(Pet, {
-        name: "Luna",
-        animal: "dog",
-        breed: "Havanese"
-      }),
-      React.createElement(Pet, {
-        name: "Luna",
-        animal: "dog",
-        breed: "Havanese"
-      }),
-      React.createElement(Pet, {
-        name: "Luna",
-        animal: "dog",
-        breed: "Havanese"
-      })
-    ]);
+    return (
+      <React.Fragment>
+        <h1>Adopt me!</h1>
+        <Pet name="Luna" animal="dog" breed="Havanese" />
+        <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+        <Pet name="Doink" animal="cat" breed="Mixed" />
+      </React.Fragment>
+    );
   }
 }
 
