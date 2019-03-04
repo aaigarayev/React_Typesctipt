260d9c2eb952fbe8de52ac4c87eb58fbf9bfa45a
Alimzhan Aigarayev <alimzhan.aigarayev@gmail.com>
2019-03-04 21:14:56 +0100

    single pet fetch, loader added and render out

--- src/Details.js
+++ src/Details.js
@@ -1,8 +1,72 @@
 import React from "react";
+import pf from "petfinder-client";
+import { navigate } from "@reach/router";
+
+// Keys that used in .env file
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 class Details extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true
+    };
+  }
+
+  componentDidMount() {
+    // Fetching single pet and passing id as a parameter
+    petfinder.pet
+      .get({
+        output: "full",
+        id: this.props.id
+      })
+      .then(data => {
+        const pet = data.petfinder.pet;
+        let breed;
+        if (Array.isArray(pet.breeds.breed)) {
+          breed = pet.breeds.breed.join(", ");
+        } else {
+          breed = pet.breeds.breed;
+        }
+
+        this.setState({
+          name: pet.name,
+          animal: pet.animal,
+          location: `${pet.contact.city}, ${pet.contact.state}`,
+          description: pet.description,
+          media: pet.media,
+          breed,
+          loading: false
+        });
+      })
+      .catch(() => {
+        // Once there is an error, it will prompt a user to the main page (navigate is taken from reach/router)
+        navigate("/");
+      });
+  }
+
   render() {
-    return <h1>Hi its Details component</h1>;
+    if (this.state.loading) {
+      return <h1>Loading...</h1>;
+    }
+
+    const { name, animal, breed, location, description } = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {location}
+          </h2>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
   }
 }
 
